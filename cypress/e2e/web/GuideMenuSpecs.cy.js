a5f278f624b70885d74cbc8ce0160c734c0d9b36
Dieferson Cruz <dieferson.cruz@outlook.com>
2022-08-26 22:01:49 -0300

    Conclusão do teste WEB

---
+++ cypress/e2e/web/GuideMenuSpecs.cy.js
@@ -0,0 +1,14 @@
+import GuidePage from "../../support/page-objects/guidePage";
+
+const guidePage =  new GuidePage;
+
+describe('Given I access the menu Guide', () => {
+    context('When I to navigate to link "/albums/1/photos"', ()=>{
+        it('Then I should see a list of albums objects', () => {
+            cy.visit('/');
+            guidePage.clickOnGuideMenu();
+            guidePage.exportDataOfPage('/albums/1/photos','albums1','report');
+            cy.compareFixtureFileDataJson('albums1-id6-DataValidation','cypress/report/albums1')
+          });
+    });
+})
---
+++ cypress/support/page-objects/guidePage.js
@@ -0,0 +1,20 @@
+class GuidePage {
+    clickOnGuideMenu(){
+        cy.get('a').contains('Guide').click();
+    }
+
+    clickLinkOnGuidadePage(hrefPage){
+        cy.get(`a[href="${hrefPage}"]`).click();
+    }
+
+    exportDataOfPage(pageUrl, fileName, exportPath, fileExtension = 'json'){
+        cy.request({
+            url: pageUrl
+        }).then((response) =>{
+            cy.writeFile(`cypress/${exportPath}/${fileName}.${fileExtension}`, response.body)
+        });
+    }
+
+}
+
+export default GuidePage;
